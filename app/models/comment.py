501c88811d419d9d9d4920764cc40cef2c4feed8
unknown <dima100421@gmail.com>
2025-02-14 15:33:38 +0300

    models have been converted to mapped

--- app/models/comment.py
+++ app/models/comment.py
@@ -1,5 +1,6 @@
-from sqlalchemy import Column, String, Integer, ForeignKey, TIMESTAMP, func
-from sqlalchemy.orm import relationship
+from sqlalchemy import String, Integer, ForeignKey, TIMESTAMP, func
+from sqlalchemy.orm import relationship, Mapped, mapped_column
+from datetime import datetime
 
 from .base import Base
 from .user import User
@@ -8,10 +9,10 @@ from .user import User
 class Comment(Base):
     __tablename__ = 'comments'
 
-    id = Column(Integer, primary_key=True, index=True)
-    user_id = Column(Integer, ForeignKey(User.id), index=True)
-    text_content = Column(String(1024), nullable=False)
-    created_at = Column(TIMESTAMP, default=func.now(), nullable=False)
+    id: Mapped[int] = mapped_column(Integer, primary_key=True, index=True)
+    user_id: Mapped[int] = mapped_column(Integer, ForeignKey(User.id), index=True)
+    text_content: Mapped[str] = mapped_column(String(1024), nullable=False)
+    created_at: Mapped[datetime] = mapped_column(TIMESTAMP, default=func.now(), nullable=False)
     
     user = relationship("User", back_populates="comments", cascade="all, delete")
 
--- app/models/user.py
+++ app/models/user.py
@@ -1,5 +1,6 @@
-from sqlalchemy import Column, Integer, String, Boolean, TIMESTAMP, func
-from sqlalchemy.orm import relationship
+from sqlalchemy import Integer, String, Boolean, TIMESTAMP, func
+from sqlalchemy.orm import relationship, Mapped, mapped_column
+from datetime import datetime
 
 from .base import Base
 
@@ -7,15 +8,15 @@ from .base import Base
 class User(Base):
     __tablename__ = 'users'
 
-    id = Column(Integer, primary_key=True, index=True)
-    username = Column(String(64), unique=True, index=True, nullable=False)
-    email = Column(String(255), unique=True, index=True, nullable=False)
-    password_hash = Column(String(256), nullable=False)
-    first_name = Column(String(64), nullable=True)
-    last_name = Column(String(64), nullable=True)
-    is_active = Column(Boolean, default=False, nullable=False)
-    date_joined = Column(TIMESTAMP, default=func.now(), nullable=False)
-    last_active_time = Column(TIMESTAMP, default=func.now(), onupdate=func.now(), nullable=False)
+    id: Mapped[int] = mapped_column(Integer, primary_key=True, index=True)
+    username: Mapped[str] = mapped_column(String(64), unique=True, index=True, nullable=False)
+    email: Mapped[str] = mapped_column(String(255), unique=True, index=True, nullable=False)
+    password_hash: Mapped[str] = mapped_column(String(256), nullable=False)
+    first_name: Mapped[str] = mapped_column(String(64), nullable=True)
+    last_name: Mapped[str] = mapped_column(String(64), nullable=True)
+    is_active: Mapped[bool] = mapped_column(Boolean, default=False, nullable=False)
+    date_joined: Mapped[datetime] = mapped_column(TIMESTAMP, default=func.now(), nullable=False)
+    last_active_time: Mapped[datetime] = mapped_column(TIMESTAMP, default=func.now(), onupdate=func.now(), nullable=False)
     
     # comments = relationship("Comment", back_populates="user", cascade="all, delete")
     posts = relationship("Post", back_populates="user", cascade="all, delete")
