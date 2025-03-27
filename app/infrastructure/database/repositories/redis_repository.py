cfaaa02ce531ddf44158cfee81d30f4079e585a2
dmitrii bortnik <dima100421@gmail.com>
2025-03-27 16:52:47 +0300

    fix(redis_repository): added json serialization

--- app/infrastructure/database/repositories/redis_repository.py
+++ app/infrastructure/database/repositories/redis_repository.py
@@ -2,7 +2,7 @@ import json
 import logging
 
 from pydantic import BaseModel
-from aioredis import Redis
+from redis.asyncio import Redis
 
 
 class RedisRepository:
@@ -18,14 +18,17 @@ class RedisRepository:
         await self.cache.delete(keys)
 
     async def get_cache(self, path: str):
-        keys = await self.cache.keys(f"{path}*")
-        if not keys:
-            logging.warning("cache keys to get not found")
-            return
-        logging.info(f"cache keys to get found")
-        return await self.cache.get(keys)
+        try:
+            value = await self.cache.get(path)
+            if value:
+                logging.info(f"cache found {path}, value={value}")
+                return json.loads(value)
+            return None
+        except Exception as e:
+            print(f"Error getting cache: {e}")
+            return None
 
     async def set_cache(self, path: str, value: BaseModel) -> None:
         json_value = json.dumps(value.model_dump())
-        await self.cache.setex(f"{path}:{value.id}", 3600, json_value)
+        await self.cache.setex(path, 3600, json_value)
         logging.info(f"value={json_value} is cached")
