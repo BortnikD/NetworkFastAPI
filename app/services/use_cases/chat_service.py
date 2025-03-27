9e4d4cd3c5c1a50842e1d63f91ad8fa0536d574a
dmitrii bortnik <dima100421@gmail.com>
2025-03-27 10:36:39 +0300

    feat(use_cases): added caching by redis

--- app/services/use_cases/chat_service.py
+++ app/services/use_cases/chat_service.py
@@ -6,15 +6,19 @@ from app.domain.repositories.chat import IChat
 from app.domain.repositories.chat_message import IChatMessage
 from app.domain.dto.chat import ChatMessageCreate, ChatMessageUpdate
 from app.infrastructure.database.models.chat import Chat, ChatMessage
+from app.infrastructure.database.repositories.redis_repository import RedisRepository
 
 
 class ChatService:
     def __init__(self, 
                  chat_port: IChat, 
                  chat_message_port: IChatMessage,
+                 cache_port: RedisRepository
                  ) -> None:
         self.chat_port = chat_port
         self.chat_message_port = chat_message_port
+        self.cache_port = cache_port
+        self.cache_path = "cache.messages"
 
 
     async def init_chat(self, current_user_id: int, target_user_id: int) -> Chat:
@@ -39,18 +43,27 @@ class ChatService:
 
     async def get_chat_messages(self, user_id: int, chat_id: int, offset: int, limit: int) -> PaginatedResponse | None:
         if await self.is_user_chat(user_id, chat_id):
-            return await self.chat_message_port.get_by_chat_id(chat_id, offset, limit)
+            cache_messages = await self.cache_port.get_cache(f'{self.cache_path}:{chat_id}')
+            if cache_messages is not None:
+                return cache_messages
+            else:
+                messages = await self.chat_message_port.get_by_chat_id(chat_id, offset, limit)
+                await self.cache_port.set_cache(f'{self.cache_path}:{chat_id}', messages)
+                return messages
         else:
             raise HTTPException(status_code=403, detail="You have no access rights")
 
 
     async def create_message(self, message: ChatMessageCreate) -> ChatMessage | None:
+        await self.cache_port.clear_cache(f'{self.cache_path}:{message.chat_id}')
         return await self.chat_message_port.save(message)
 
 
     async def update_message(self, message: ChatMessageUpdate, current_user_id: int) -> ChatMessage:
+        await self.cache_port.clear_cache(f'{self.cache_path}:{message.chat_id}')
         return await self.chat_message_port.update(message, current_user_id)
 
 
     async def delete_message(self, message_id: int, current_user_id: int) -> None:
+        # TODO добавить очистку кэша, для этого нужно будет дополнительно получать chat_id
         await self.chat_message_port.delete(message_id, current_user_id)
