92c1d30a304f87cf1fa9f1aa4edb9859d5844900
dmitrii bortnik <dima100421@gmail.com>
2025-04-01 16:06:12 +0300

    refactor(subscriptions): refactor annotations and added validation in repositories

--- app/api/fast_api/routes/endpoints/http/subscription_controller.py
+++ app/api/fast_api/routes/endpoints/http/subscription_controller.py
@@ -1,8 +1,8 @@
 from fastapi import APIRouter, Depends, Query, HTTPException
 from typing import Annotated
 
-from app.domain.dto.pagination import LikePagination
-from app.domain.dto.subscription import SubscriptionPublic
+from app.domain.dto.pagination import LikePagination, PaginatedResponse
+from app.domain.entities.subscription import Subscription
 from app.domain.exceptions.base import AccessError
 from app.domain.exceptions.subscription import (
     SubscriptionAlreadyExists,
@@ -18,12 +18,12 @@ from app.infrastructure.database.models import User
 router = APIRouter(prefix='/subscriptions')
 
 
-@router.get('/{user_id}', response_model=list[SubscriptionPublic])
+@router.get('/{user_id}')
 async def get_subscriptions_by_user_id(
-    user_id: int,
-    pagination: Annotated[LikePagination, Query()],
-    service: SubscriptionService = Depends(get_subscription_service)
-):
+        user_id: int,
+        pagination: Annotated[LikePagination, Query()],
+        service: SubscriptionService = Depends(get_subscription_service)
+        ) -> PaginatedResponse:
     """Получить список подписок пользователя."""
     try:
         return await service.get_subscriptions_by_user_id(user_id, pagination.offset, pagination.limit)
@@ -31,12 +31,12 @@ async def get_subscriptions_by_user_id(
         raise HTTPException(status_code=404, detail=e.message)
 
 
-@router.post('/', response_model=SubscriptionPublic)
+@router.post('/')
 async def create_subscription(
-    followed_user_id: int,
-    current_user: User = Depends(get_current_active_user),
-    service: SubscriptionService = Depends(get_subscription_service)
-):
+        followed_user_id: int,
+        current_user: User = Depends(get_current_active_user),
+        service: SubscriptionService = Depends(get_subscription_service)
+        ) -> Subscription:
     """Создать подписку на пользователя."""
     try:
         return await service.save(current_user.id, followed_user_id)
@@ -46,10 +46,10 @@ async def create_subscription(
 
 @router.delete('/{subscription_id}')
 async def delete_subscription(
-    subscription_id: int,
-    current_user: User = Depends(get_current_active_user),
-    service: SubscriptionService = Depends(get_subscription_service)
-):
+        subscription_id: int,
+        current_user: User = Depends(get_current_active_user),
+        service: SubscriptionService = Depends(get_subscription_service)
+        ) -> None:
     """Удалить подписку по её ID."""
     try:
         return await service.delete(subscription_id, current_user.id)
