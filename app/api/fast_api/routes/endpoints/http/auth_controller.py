0bda46513032857d7894d288305760640d3ff7b0
dmitrii bortnik <dima100421@gmail.com>
2025-04-03 10:44:45 +0300

    refactor(auth_controller.py): using UserDB model instead of User db_model, upgrade annotations

--- app/api/fast_api/routes/endpoints/http/auth_controller.py
+++ app/api/fast_api/routes/endpoints/http/auth_controller.py
@@ -4,12 +4,12 @@ from fastapi import APIRouter, Depends, HTTPException, status
 from fastapi.security import OAuth2PasswordRequestForm
 
 from app.dependencies.services.user import get_user_service
-from app.infrastructure.database.models.user import User
 from app.services.core_services.user_service import UserService
 from app.dependencies.auth import get_current_active_user
 from app.infrastructure.settings.security import create_access_token, verify_password
 from app.infrastructure.settings.config import ACCESS_TOKEN_EXPIRE_DAYS
-from app.domain.dto.user import UserPublic
+from app.domain.dto.user import UserDB
+from app.domain.entities.user import User as UserEntity
 from app.domain.dto.auth import Token
 
 router = APIRouter()
@@ -19,18 +19,18 @@ async def authenticate_user(
     email: str,
     password: str,
     user_service: UserService = Depends(get_user_service)
-) -> User | None:
+    ) -> UserDB | None:
     user = await user_service._get_by_email(email)
     if not user or not verify_password(password, user.password_hash):
         return None
     return user
 
 
-@router.post("/token", response_model=Token)
+@router.post("/token")
 async def login_for_access_token(
     form_data: Annotated[OAuth2PasswordRequestForm, Depends()],
     user_service: UserService = Depends(get_user_service)
-):
+    ) -> Token:
     user = await authenticate_user(form_data.username, form_data.password, user_service)
     if not user:
         raise HTTPException(
@@ -44,11 +44,11 @@ async def login_for_access_token(
     return Token(access_token=access_token, token_type="bearer")
 
 
-@router.get("/users/me/", response_model=UserPublic)
+@router.get("/users/me/")
 async def read_users_me(
-    current_user: Annotated[User, Depends(get_current_active_user)],
+    current_user: UserDB = Depends(get_current_active_user),
     user_service: UserService = Depends(get_user_service)
-):
+    ) -> UserEntity:
     """Получает информацию о текущем авторизованном пользователе."""
     user = await user_service.get_by_id(current_user.id)
     if not user:
