c1b92d0abb69e5f486971ed767d316685792ebb1
dmitrii bortnik <dima100421@gmail.com>
2025-04-05 15:38:42 +0300

    feat(subscription_service.py, dependencies.subscription.py): added caching

--- app/dependencies/services/subscription.py
+++ app/dependencies/services/subscription.py
@@ -1,10 +1,15 @@
 from fastapi import Depends
 
+from app.infrastructure.database.redis import RedisClient
 from app.services.core_services.subscription_service import SubscriptionService
 from app.infrastructure.database.repositories.subscription_repository import SubscriptionRepository
+from app.infrastructure.database.repositories.redis_repository import RedisRepository
 from app.dependencies.db import get_db
 
 
-def get_subscription_service(db=Depends(get_db)) -> SubscriptionService:
+async def get_subscription_service(db=Depends(get_db)) -> SubscriptionService:
     subscription = SubscriptionRepository(db)
-    return SubscriptionService(subscription)
+    client = RedisClient()
+    redis = await client.get_redis()
+    redis_repo = RedisRepository(redis)
+    return SubscriptionService(subscription, redis_repo)
--- app/services/core_services/subscription_service.py
+++ app/services/core_services/subscription_service.py
@@ -1,20 +1,36 @@
 from app.domain.dto.pagination import PaginatedResponse
 from app.domain.entities.subscription import Subscription
+from app.domain.repositories.redis import IRedis
 from app.domain.repositories.subscription import ISubscription
 from app.domain.exceptions.subscription import SelfSubscriptionError
 
 
 class SubscriptionService:
-    def __init__(self, subscription_port: ISubscription):
+    def __init__(self,
+                 subscription_port: ISubscription,
+                 cache_port: IRedis
+                 ):
         self.subscription_port = subscription_port
+        self.cache_port = cache_port
+        self.cache_path = "cache.subscriptions"
 
     async def save(self, follower_id: int, followed_user_id: int) -> Subscription:
         if follower_id == followed_user_id:
             raise SelfSubscriptionError('You cannot subscribe to yourself')
-        return await self.subscription_port.save(follower_id, followed_user_id)
+        result =  await self.subscription_port.save(follower_id, followed_user_id)
+        await self.cache_port.clear_cache(f'{self.cache_path}:{follower_id}:*')
+        await self.cache_port.clear_cache(f'{self.cache_path}:{followed_user_id}:*')
+        return result
 
     async def get_subscriptions_by_user_id(self, user_id: int, offset: int, limit: int) -> PaginatedResponse:
-        return await self.subscription_port.get_subscriptions_by_user_id(user_id, offset, limit)
+        cache_key = f'{self.cache_path}:{user_id}:{offset}:{limit}'
+        subscriptions = await self.cache_port.get_cache(cache_key)
+        if subscriptions:
+            return PaginatedResponse(**subscriptions)
+        subscriptions = await self.subscription_port.get_subscriptions_by_user_id(user_id, offset, limit)
+        await self.cache_port.set_cache(cache_key, subscriptions)
+        return subscriptions
 
     async def delete(self, subscription_id: int, current_user_id: int) -> None:
         await self.subscription_port.delete(subscription_id, current_user_id)
+        await self.cache_port.clear_cache(f'{self.cache_path}:{current_user_id}:*')
