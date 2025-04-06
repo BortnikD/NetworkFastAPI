f0f019c75608a0c892b1e5a66696996b29d73a58
dmitrii bortnik <dima100421@gmail.com>
2025-04-06 18:44:01 +0300

    refactor(dependencies.services): refactor with redis depends

--- app/dependencies/services/chat.py
+++ app/dependencies/services/chat.py
@@ -1,17 +1,13 @@
 from fastapi import Depends
 
+from app.dependencies.redis import get_redis
 from app.services.core_services.chat_service import ChatService
 from app.infrastructure.database.repositories.chat_repository import ChatRepository
 from app.infrastructure.database.repositories.chat_message_repository import ChatMessageRepository
-from app.infrastructure.database.repositories.redis_repository import RedisRepository
-from app.infrastructure.database.redis import RedisClient
 from app.dependencies.db import get_db
 
 
-async def get_chat_service(db=Depends(get_db)) -> ChatService:
+async def get_chat_service(db=Depends(get_db), cache=Depends(get_redis)) -> ChatService:
     chat_repo = ChatRepository(db)
     chat_message_repo = ChatMessageRepository(db)
-    client = RedisClient()
-    redis = await client.get_redis()
-    redis_repo = RedisRepository(redis)
-    return ChatService(chat_repo, chat_message_repo, redis_repo)
+    return ChatService(chat_repo, chat_message_repo, cache)
--- app/dependencies/services/subscription.py
+++ app/dependencies/services/subscription.py
@@ -1,15 +1,11 @@
 from fastapi import Depends
 
-from app.infrastructure.database.redis import RedisClient
+from app.dependencies.redis import get_redis
 from app.services.core_services.subscription_service import SubscriptionService
 from app.infrastructure.database.repositories.subscription_repository import SubscriptionRepository
-from app.infrastructure.database.repositories.redis_repository import RedisRepository
 from app.dependencies.db import get_db
 
 
-async def get_subscription_service(db=Depends(get_db)) -> SubscriptionService:
-    subscription = SubscriptionRepository(db)
-    client = RedisClient()
-    redis = await client.get_redis()
-    redis_repo = RedisRepository(redis)
-    return SubscriptionService(subscription, redis_repo)
+async def get_subscription_service(db=Depends(get_db), cache=Depends(get_redis)) -> SubscriptionService:
+    repository = SubscriptionRepository(db)
+    return SubscriptionService(repository, cache)
